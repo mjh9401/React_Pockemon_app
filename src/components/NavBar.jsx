67f3f0aca310ad4ca6a5e634af4fd7df735273b0
mjh9401@naver.com <mjh9401@naver.com>
2024-03-18 23:29:25 +0900

    로그인 & 로그아웃

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,45 @@
-import React, { useEffect, useState } from 'react'
-import styled from 'styled-components'
+import { GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router';
+import styled from 'styled-components';
+import app from '../firebase';
 
 const NavBar = () => {
+    const auth = getAuth(app);
+    const provider = new GoogleAuthProvider();
+
     const [show, setShow] = useState(false);
-    console.log(show);
+    const [userData, setUserData] = useState({});
+
+    // useLocation이 URL 정보를 가지고 있음
+    const {pathname} = useLocation();
+    // 네비게이트 => 화면이동
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+       const unsubcribe= onAuthStateChanged(auth,(user)=>{
+        if(!user){
+            navigate("/login");
+        }else if(user && pathname ==='/login'){
+            navigate("/");
+        }
+      })
+    
+      return () => {
+        unsubcribe();
+      }
+    }, [pathname])
+    
+    
+    const handleAuth = () => {
+        signInWithPopup(auth,provider)
+        .then((result) =>{
+            setUserData(result.user);
+        })
+        .catch(error =>{
+            console.error(error);
+        })
+    }
 
     const listener = () => {
         if(window.scrollY > 50){
@@ -15,12 +51,21 @@ const NavBar = () => {
 
     useEffect(() => {
       window.addEventListener('scroll', listener)
-    
+        
+      // 컴포넌트를 사용 안될 때 제거하는 곳
       return () => {
         window.removeEventListener('scroll', listener)
       }
     }, [])
     
+    const handleLogout = () => {
+        signOut(auth).then(() => {
+            setUserData({});
+        })
+        .catch(error => {
+            alert(error.message);
+        })
+    }
 
     return (
         <NavWraper show={show}>
@@ -31,9 +76,79 @@ const NavBar = () => {
                 onClick={()=>(window.location.href = "/")}
             />  
             </Logo>
+            {pathname === '/login' ? (
+                <Login onClick={handleAuth}>로그인</Login>
+            ): 
+            <SignOut>
+                <UserImg 
+                    src={userData.photoURL}
+                    alt='user Photo'
+                />
+                <DropDown>
+                    <span onClick={handleLogout}>Sign out</span>
+                </DropDown>
+            </SignOut>      
+            }
         </NavWraper>
     )
 }
+
+const UserImg = styled.img`
+    border-radius : 50%;
+    widht : 100%;
+    height : 100%;
+`
+
+
+const DropDown = styled.div`
+    position : absolute;
+    top : 48px;
+    right : 0px;
+    background : rgb(19,19,19);
+    border : 1px solid rgba(151,151,151, 0.34);
+    border-radius : 4px;
+    box-shadow : rgb(0,0,0 / 50%) 0px 0px 18px 0px;
+    padding : 10px;
+    font-size : 14px;
+    letter-spacing : 3px;
+    width : 100px;
+    opacity : 0;
+    color : white;
+`
+
+const SignOut = styled.div`
+    position : relative;
+    height : 48px;
+    width : 48px;
+    display : flex;
+    cursor : pointer;
+    align-items : center;
+    justify-contents : center;
+
+    &:hover {
+        ${DropDown}{
+            opacity : 1;
+            transition-duration : 1s;
+        }
+    }
+`
+
+const Login = styled.a`
+    background-color: rgba(0, 0, 0, 0.6);
+    padding: 8px 16px;
+    text-transform : uppercase;
+    letter-spacing : 1.55px;
+    border : 1px solid #f9f9f9;
+    border-radius : 4px;
+    transition : all 0.2s ease 0s;
+    color : white;
+
+    &:hover {
+        background-color : #f9f9f9;
+        color : #000;
+        border-color : transparent;
+    }
+`
 
 const Image = styled.img`
     cursor : pointer;
