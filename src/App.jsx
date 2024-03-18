4060dd177e3f3e6e9cc8515277743002ce97c76f
mjh9401 <mjh9401@naver.com>
2024-03-18 16:51:08 +0900

    FireBase, 로그인UI 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
 import MainPage from './pages/MainPage'
 import DetailPage from './pages/DetailPage'
+import LoginPage from './pages/LoginPage'
+import NavBar from './components/NavBar'
+
+const LayOut = () =>{
+  return(
+    <>
+      <NavBar/>
+      <br/>
+      <br/>
+      <br/>
+      <Outlet/>
+    </>
+  )
+}
+
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<MainPage/>}/>
-        <Route path='/pokemon/:id' element={<DetailPage/>}/>
+        <Route path='/' element={<LayOut/>}>
+          <Route index element={<MainPage/>}/>
+          <Route path='/pokemon/:id' element={<DetailPage/>}/>
+          <Route path='login' element={<LoginPage/>}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDC-FioyKrfO9jQlLaWEqSHsM2PsjoAgns",
+  authDomain: "react-poke-app-f7413.firebaseapp.com",
+  projectId: "react-poke-app-f7413",
+  storageBucket: "react-poke-app-f7413.appspot.com",
+  messagingSenderId: "663176952599",
+  appId: "1:663176952599:web:7385c8e246f2af3df89552"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
