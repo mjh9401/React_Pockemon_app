fc58627aa8be939b370dfb911fa94bebc1a660a8
mjh9401 <mjh9401@naver.com>
2024-03-18 18:12:47 +0900

    Navbar 컴포넌트 생성

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
 
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
+    const [show, setShow] = useState(false);
+    console.log(show);
+
+    const listener = () => {
+        if(window.screenY > 50){
+            setShow(true);
+        }else{
+            setShow(false);
+        }
+    }
+
+    useEffect(() => {
+      window.addEventListener('scroll', listener);
+    
+      return () => {
+        window.removeEventListener('scroll', listener);
+      }
+    }, [])
+    
+
+    return (
+        <NavWraper show={show}>
+            <Logo>
+            <Image
+                alt="Poke logo"
+                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
+                onClick={()=>(window.location.href = "/")}
+            />  
+            </Logo>
+        </NavWraper>
+    )
 }
+
+const Image = styled.img`
+    cursor : pointer;
+    width: 100%;
+`
+
+const Logo = styled.a`
+    padding: 0;
+    width: 50px;
+    margin-top: 4px;
+`
+
+const NavWraper = styled.nav`
+    position : fixed;
+    top: 0;
+    left : 0;
+    right : 0;
+    height: 70px;
+    display : flex;
+    background-color : ${props => props.show ? "#090b13" : "transparent"};
+    justify-content : space-between;
+    align-items : center;
+    padding : 0 36px;
+    letter-spacing : 16px;
+    z-index : 100;
+`
 
 export default NavBar
